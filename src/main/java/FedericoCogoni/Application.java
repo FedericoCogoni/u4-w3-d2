675d163b11a9d5677f085a4bd9847ffd5f01a69b
Federico Cogoni <federicocogoni90@gmail.com>
2024-06-04 16:58:11 +0200

    findBy/deletById

--- src/main/java/FedericoCogoni/Application.java
+++ src/main/java/FedericoCogoni/Application.java
@@ -15,10 +15,30 @@ public class Application {
     public static void main(String[] args) {
             System.out.println("Hello World!");
             EntityManager em = emf.createEntityManager();
-            EventoDAO db = new EventoDAO(em);
+            EventoDAO sd = new EventoDAO(em);
 
 
-        Evento prova = new Evento("prima prova", LocalDate.of(2022, 10, 11), "descrizione prova", TipoEvento.PRIVATO, 12);
-        db.save(prova);
-        }
+       /* Evento prova4= new Evento("prova4", LocalDate.of(2022, 10, 11), "descrizione prova", TipoEvento.PRIVATO, 12);
+        Evento prova1 = new Evento("prova1", LocalDate.of(2022, 10, 11), "descrizione prova", TipoEvento.PRIVATO, 12);
+        Evento prova2 = new Evento("prova2", LocalDate.of(2022, 10, 11), "descrizione prova", TipoEvento.PRIVATO, 12);
+        Evento prova3 = new Evento("prova3", LocalDate.of(2022, 10, 11), "descrizione prova", TipoEvento.PRIVATO, 12);
+        sd.save(prova1);
+        sd.save(prova2);
+        sd.save(prova3);
+        sd.save(prova4);
+        System.out.println("evento " + prova1.getTitolo() + " creato");
+        System.out.println("evento " + prova2.getTitolo() + " creato");
+        System.out.println("evento " + prova3.getTitolo() + " creato");
+        System.out.println("evento " + prova4.getTitolo() + " creato");*/
+
+        Evento prova1FromDB = sd.findById(1502);
+        System.out.println(prova1FromDB.getTitolo() + " " + prova1FromDB.getData_evento());
+
+        sd.deleteById(1502);
+        System.out.println("evento" + prova1FromDB + "eliminato");
+        
+        em.close();
+        emf.close();
+
+    }
     }
--- src/main/java/FedericoCogoni/dao/EventoDAO.java
+++ src/main/java/FedericoCogoni/dao/EventoDAO.java
@@ -17,4 +17,17 @@ private final EntityManager em;
         em.persist(evento);
         transaction.commit();
     }
+
+    public Evento findById(long id) {
+        Evento evento = em.find(Evento.class, id);
+    return evento;
+    }
+
+    public void deleteById (long id) {
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Evento evento = em.find(Evento.class, id);
+        em.remove(evento);
+        tx.commit();
+    }
 }
